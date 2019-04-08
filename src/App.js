1442d89f2176afbfb67423a8b90a877cc717c242
steve <steven@eventprojection.co.uk>
2019-04-08 21:44:09 +0100

    #4 single image for mobile use

--- src/App.js
+++ src/App.js
@@ -8,37 +8,31 @@ import rotk from './images/ROTK.jpg';
 import pf from './images/PF.jpg';
 
 
-//Display them in 2 rows (3 covers per row).
-//Titles will be set as big headings, aligned to the middle (horizontally), and with 30px margin each
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
-        <br /><br /><br /><br />
         <div className='container'>
           <div className='item'>
-            <img src={ssr} alt='Shawshank redemption'/>
+            <img src={ssr} alt='Shawshank redemption' />
           </div>
           <div className='item'>
-            <img src={tgf} alt='The Godfather'/>
+            <img src={tgf} alt='The Godfather' />
           </div>
           <div className='item'>
-            <img src={tdk} alt='the Dark Knight'/>
+            <img src={tdk} alt='the Dark Knight' />
           </div>
         </div>
 
         <div className="container">
           <div className='item'>
-            <img src={tgf2} alt='The Godfather Part 2'/>
+            <img src={tgf2} alt='The Godfather Part 2' />
           </div>
           < div className='item'>
-            <img src={rotk} alt='Return Of The King'/>
+            <img src={rotk} alt='Return Of The King' />
           </div>
           <div className='item'>
-            <img src={pf} alt='Pulp Fiction'/>
+            <img src={pf} alt='Pulp Fiction' />
           </div>
         </div>
 
