e1a1e71977f0a4c7f040422e4d95a30cbac4ce50
steve <steven@eventprojection.co.uk>
2019-04-08 20:16:28 +0100

    #3 Ading images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
-//import theMachinist from './images/theMachinist.jpg';
 import './App.css';
+import ssr from './images/SSR.jpg';
+import tgf from './images/TGF.jpg';
+import tdk from './images/TDK.jpg';
+import tgf2 from './images/TGF2.jpg';
+import rotk from './images/ROTK.jpg';
+import pf from './images/PF.jpg';
+
 
 //Display them in 2 rows (3 covers per row).
 //Titles will be set as big headings, aligned to the middle (horizontally), and with 30px margin each
@@ -10,20 +16,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<img src={theMachinist} alt="The Machist cover"/>*/}
+       
         <br /><br /><br /><br />
-
-        <div className="Container">
-          <div><h1>The Shawshank Redeption</h1></div>
-          <div><h1>The Godfather</h1></div>
-          <div><h1>The Dark Knight</h1></div>
-
+        <div className='container'>
+          <div className='item'>
+            <img src={ssr} alt='Shawshank redemption'/>
+          </div>
+          <div className='item'>
+            <img src={tgf} alt='The Godfather'/>
+          </div>
+          <div className='item'>
+            <img src={tdk} alt='the Dark Knight'/>
+          </div>
         </div>
-        <div className="Container">
-          <div><h1>The Godfather Part 2</h1></div>
-          <div><h1>Return Of The King</h1></div>
-          <div><h1>Pulp Fiction</h1></div>
+
+        <div className="container">
+          <div className='item'>
+            <img src={tgf2} alt='The Godfather Part 2'/>
+          </div>
+          < div className='item'>
+            <img src={rotk} alt='Return Of The King'/>
+          </div>
+          <div className='item'>
+            <img src={pf} alt='Pulp Fiction'/>
+          </div>
         </div>
+
       </div>
 
     );
