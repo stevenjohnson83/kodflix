2e8aa1cd37ddeffeca858edaa722af0e79b1d6b3
steve <steven@eventprojection.co.uk>
2019-04-11 16:55:17 +0100

    # 7 create component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Stack from './Stack.js';
 import ssr from './images/SSR.jpg';
 import tgf from './images/TGF.jpg';
 import tdk from './images/TDK.jpg';
@@ -10,53 +11,18 @@ import pf from './images/PF.jpg';
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-
+      <div className="App">
         <div className='container'>
-          <div className='item'>
-            <img src={ssr} alt='Shawshank redemption' />
-            <div className='overlay'>
-              <h2>The Shawshank Redemption</h2>
-            </div>
-          </div>
-
-          <div className='item'>
-            <img src={tgf} alt='The Godfather' />
-            <div className='overlay'>
-              <h2>The Godfather</h2>
-            </div>
-          </div>
-          <div className='item'>
-            <img src={tdk} alt='The Dark Knight' />
-            <div className='overlay'>
-              <h2>The Dark Knight</h2>
-            </div>
-          </div>
+          <Stack name='Shawshank redemption' img={ssr} />
+          <Stack name='The Godfather' img={tgf} />
+          <Stack name='The Dark Knight' img={tdk} />
         </div>
-
-        <div className="container">
-          <div className='item'>
-            <img src={tgf2} alt='The Godfather Pt 2' />
-            <div className='overlay'>
-              <h2>The Godfather Pt 2</h2>
-            </div>
-          </div>
-          < div className='item'>
-            <img src={rotk} alt='Return Of The King' />
-            <div className='overlay'>
-              <h2>Return Of The King</h2>
-            </div>
-          </div>
-          <div className='item'>
-            <img src={pf} alt='Pulp Fiction' />
-            <div className='overlay'>
-              <h2>Pulp Fiction</h2>
-            </div>
-          </div>
+        <div className='container'>
+          <Stack name='The Godfather Pt 2' img={tgf2} />
+          <Stack name='Return Of The King' img={rotk} />
+          <Stack name='Pulp Fiction' img={pf} />
         </div>
-
       </div>
-
     );
   }
 }
---
+++ src/Stack.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Stack(props) {
+    return (
+      <div className='item'>
+        <img src={props.img} alt={props.name}/>
+        <h1 className='title overlay'> {props.name} </h1>
+      </div>
+    );
+  }
