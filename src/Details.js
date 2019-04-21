b600f08e69279b1aaee4443d02f223a6a9e0698b
steve <steven@eventprojection.co.uk>
2019-04-21 12:31:32 +0100

    # 13Introduce component state

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Details(props) {
+
+
+export default class Details extends Component {
+    
+    constructor(){
+        super();
+        this.state = {
+            welcomeMessage: 'work in progress'
+        };
+    }
+
+    componentDidMount(){
+        setTimeout(() =>{
+            this.setState({
+                welcomeMessage: 'test change message'
+            });
+            
+
+        }, 3000)
+    }
+
+
+    render(){
     return (
         <div>
-            <h1>'work in progress'</h1>
+           <h1>{this.state.welcomeMessage}</h1>
             <Link to='/'>Back to Gallery </Link>
         </div>
     );
+}
 }
